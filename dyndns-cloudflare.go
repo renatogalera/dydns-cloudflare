b3ac1f2be6a223c3dbe2e16d7e8f75d4ad7366f0
renatoguilhermini <45680168+renatoguilhermini@users.noreply.github.com>
2019-08-30 13:15:47 -0300

    Update dyndns-cloudflare.go

--- dyndns-cloudflare.go
+++ dyndns-cloudflare.go
@@ -3,14 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"strings"
-	"github.com/bogdanovich/dns_resolver"
-	cloudflare "github.com/cloudflare/cloudflare-go"
+	"github.com/GlenDC/go-external-ip"
+	"github.com/cloudflare/cloudflare-go"
 	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 var DOMAIN string
@@ -20,93 +17,78 @@ var SUBDOMAIN string
 var NEWIPADDR string
 
 func loadConfig() error {
-	// Checa se arquivo config.env existe
+
 	err := godotenv.Load("config.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	// Coletando variáveis
+
 	DOMAIN = os.Getenv("DOMAIN")
 	if DOMAIN == "" {
-		msg := fmt.Sprintf("É necessário configurar variável DOMAIN")
+		msg := fmt.Sprintf("Please, set variable DOMAIN")
 		return errors.New(msg)
 	}
+
 	CF_API_KEY = os.Getenv("CF_API_KEY")
 	if CF_API_KEY == "" {
-		msg := fmt.Sprintf("É necessário configurar variável CF_API_KEY")
+		msg := fmt.Sprintf("Please, set variable CF_API_KEY")
 		return errors.New(msg)
 	}
+
 	CF_API_EMAIL = os.Getenv("CF_API_EMAIL")
 	if CF_API_EMAIL == "" {
-		msg := fmt.Sprintf("É necessário configurar variável CF_API_EMAIL")
+		msg := fmt.Sprintf("Please, set variable CF_API_EMAIL")
 		return errors.New(msg)
 	}
+
 	SUBDOMAIN = os.Getenv("SUBDOMAIN")
 	if SUBDOMAIN == "" {
-		msg := fmt.Sprintf("É necessário configurar variável SUBDOMAIN")
+		msg := fmt.Sprintf("Please, set variable SUBDOMAIN")
 		return errors.New(msg)
 	}
+
 	NEWIPADDR = os.Getenv("NEWIPADDR")
 	if NEWIPADDR == "" {
-		msg := fmt.Sprintf("É necessário configurar variável NEWIPADDR")
+		msg := fmt.Sprintf("Please, set variable NEWIPADDR")
 		return errors.New(msg)
 	}
 	return nil
 }
 
-func checaIPDNS(target string) {
-	resolver := dns_resolver.New([]string{"1.1.1.1"})
-	resolver.RetryTimes = 5
-	ip, err := resolver.LookupHost(SUBDOMAIN + "." + DOMAIN)
-	
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	
-	for _, ips := range ip {
-		dnsip := ips.String()
-		if target == dnsip {
-			fmt.Println("IP não mudou!")
-			os.Exit(0)
-		}else{
-			dynDNS(target)
-		}
-	}
-}
-
 func dynDNS(target string) {
+
 	api, err := cloudflare.New(CF_API_KEY, CF_API_EMAIL)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	zoneID, err := api.ZoneIDByName(DOMAIN)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	
+
 	newRecord := cloudflare.DNSRecord{
 		Type:    "A",
 		Name:    SUBDOMAIN + "." + DOMAIN,
 		Content: target,
 	}
-	
+
 	updateRecord(zoneID, api, &newRecord)
-	log.Println("Setando entrada DNS:", newRecord.Name, newRecord.Content, "\n")
+	log.Println("IP changed, setting DNS entry:", newRecord.Name, newRecord.Content, "\n")
 }
 
 func updateRecord(zoneID string, api *cloudflare.API, newRecord *cloudflare.DNSRecord) {
+
 	dns := cloudflare.DNSRecord{Type: newRecord.Type, Name: newRecord.Name}
 	OLDRECORDS, err := api.DNSRecords(zoneID, dns)
-	
+
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	
+
 	if len(OLDRECORDS) == 1 {
-		// Update
 		err := api.UpdateDNSRecord(zoneID, OLDRECORDS[0].ID, *newRecord)
 		if err != nil {
 			log.Fatal(err)
@@ -114,7 +96,7 @@ func updateRecord(zoneID string, api *cloudflare.API, newRecord *cloudflare.DNSR
 		}
 		return
 	}
-	
+
 	_, err = api.CreateDNSRecord(zoneID, *newRecord)
 	if err != nil {
 		log.Fatal(err)
@@ -123,36 +105,48 @@ func updateRecord(zoneID string, api *cloudflare.API, newRecord *cloudflare.DNSR
 }
 
 func getMyIP(protocol int) string {
-	
-	var target string
-	
-	if protocol == 4 {
-		target = "http://ifconfig.me/ip"
-		//} else if protocol == 6 {
-		//	target = "http://ifconfig.me/ip" //Alterar para fonte ipv6
-	}else{
-		os.Exit(0)
+
+	consensus := externalip.DefaultConsensus(nil, nil)
+	ip, err := consensus.ExternalIP()
+
+	if err != nil {
+		fmt.Println("Error collecting external IP") // print IPv4/IPv6 in string format
 	}
 
-	resp, err := http.Get(target)
-
-	if err == nil {
-		contents, err := ioutil.ReadAll(resp.Body)
-		if err == nil {
-			defer resp.Body.Close()
-			return strings.TrimSpace(string(contents))
-		}
-
-	}
+	target := ip.String()
 	return target
 }
 
 func main() {
+
 	err := loadConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	target := getMyIP(4)
-	checaIPDNS(target)
+
+	api, err := cloudflare.New(CF_API_KEY, CF_API_EMAIL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	zoneID, err := api.ZoneIDByName(DOMAIN)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	ipdns := cloudflare.DNSRecord{Name: SUBDOMAIN + "." + DOMAIN}
+	recs, err := api.DNSRecords(zoneID, ipdns)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, r := range recs {
+		if getMyIP(4) == r.Content {
+			fmt.Println("IP has not changed!")
+			os.Exit(0)
+		}else{
+			dynDNS(getMyIP(4))
+		}
+	}
 }
